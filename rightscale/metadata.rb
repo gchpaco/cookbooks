717acb376bda15665fecaaea01d6e80291e6e7a5
Graham Hughes <graham@rightscale.com>
2010-12-07 17:53:01 -0800

    Add rightscale cookbook with patches to right_popen.

---
+++ rightscale/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale, Inc."
+maintainer_email "graham@rightscale.com"
+license          "Apache 2.0"
+description      "Patches to rightscale stuff"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+recipe "rightscale::right_popen", "Upgrades right_popen to version 1.0.11"
