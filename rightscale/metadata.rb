5ab1745fda8a434de78bba537ddb886b057cf69a
Graham Hughes <graham@rightscale.com>
2011-02-22 17:35:31 -0800

    Add Cary's emacs install/uninstall

--- rightscale/metadata.rb
+++ rightscale/metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "graham@rightscale.com"
 license          "Apache 2.0"
 description      "Patches to rightscale stuff"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.2"
 
 recipe "rightscale::right_popen", "Upgrades right_popen to version 1.0.11"
+recipe "rightscale::abuse_emacs", "Uninstall and install emacs"
---
+++ rightscale/recipes/abuse_emacs.rb
@@ -0,0 +1,5 @@
+# Cookbook Name:: rightscale
+# Recipe:: abuse_emacs
+package "emacs" { action :remove }
+package "emacs" { action :purge }
+package "emacs"
