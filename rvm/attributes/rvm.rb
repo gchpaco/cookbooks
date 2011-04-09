00f2758071357bcd88fff8480edd6f883b1a4261
Graham Hughes <graham@rightscale.com>
2011-04-08 17:32:31 -0700

    Use Net::HTTP directly, disabling SSL (ick).

--- rvm/attributes/rvm.rb
+++ rvm/attributes/rvm.rb
@@ -1,3 +1,14 @@
+require 'net/https'
+require 'uri'
+
 default[:rvm][:releases] = "https://rvm.beginrescueend.com/releases/"
-default[:rvm][:version] = open("#{node[:rvm][:releases]}/latest-version.txt").read.chomp
+uri = URI.join(node[:rvm][:releases], "latest-version.txt")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true if uri.scheme == "https"
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+http.start {
+  http.request_get(uri.path) {|res|
+    default[:rvm][:version] = res.body.chomp
+  }
+}
 default[:rvm][:rubies] = ["1.8.7", "1.9.2"]
